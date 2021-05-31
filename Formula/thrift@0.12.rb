305ebdd468f6c27eb8b04cf7538c5aba21500a04
Roccoon <me@wanghaoyu.cn>
2021-05-31 21:42:48 +0800

    refactor: ♻️ 升级brew格式

--- Formula/thrift@0.12.rb
+++ Formula/thrift@0.12.rb
@@ -6,11 +6,10 @@ class ThriftAT012 < Formula
   revision 1
 
   bottle do
-    cellar :any
-    sha256 "17605f1674a5bc1f374f13137db550c51181e7eebae59513444d0f46032a2a78" => :catalina
-    sha256 "ead278adf991ed6056b97806f5a7815f76340492d00b39801c863e907826a2ec" => :mojave
-    sha256 "f2a1fcbee158d5478f786a1ff7667c65061e15f8a0ebecdbc69e748c184cc8ef" => :high_sierra
-    sha256 "3b554722d5011a8aa1906046d4d65b3482a121baf36c737aca4de1d270171e42" => :sierra
+    sha256 cellar: :any, catalina:    "17605f1674a5bc1f374f13137db550c51181e7eebae59513444d0f46032a2a78"
+    sha256 cellar: :any, mojave:      "ead278adf991ed6056b97806f5a7815f76340492d00b39801c863e907826a2ec"
+    sha256 cellar: :any, high_sierra: "f2a1fcbee158d5478f786a1ff7667c65061e15f8a0ebecdbc69e748c184cc8ef"
+    sha256 cellar: :any, sierra:      "3b554722d5011a8aa1906046d4d65b3482a121baf36c737aca4de1d270171e42"
   end
 
   head do
